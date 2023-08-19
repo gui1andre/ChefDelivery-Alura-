5b978daf102b9ebefc0d9ca27454d415b33cdd3c
Guilherme Andre <gui1andre89@gmail.com>
2023-08-18 21:08:11 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Guilherme Andre de Souza on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
