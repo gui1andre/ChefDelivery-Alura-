9b5b9eaea4a32d1b6dd46ff20db0393a222ca71a
Guilherme Andre <gui1andre89@gmail.com>
2023-08-19 23:18:24 -0300

    refatoracao

---
+++ ChefDelivery/StoresView/StoreDetailview/StoreDetailHeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  StoreDetailHeaderView.swift
+//  ChefDelivery
+//
+//  Created by Guilherme Andre de Souza on 19/08/23.
+//
+
+import SwiftUI
+
+struct StoreDetailHeaderView: View {
+    let store: StoreType
+    var body: some View {
+        VStack{
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            HStack{
+                Text(store.name)
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Image(store.logoImage)
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            HStack{
+                Text(store.location)
+                Spacer()
+                ForEach(1...store.stars, id: \.self){_ in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .font(.caption)
+                }
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct StoreDetailHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoreDetailHeaderView(store: storesMock[0])
+            .previewLayout(.sizeThatFits)
+    }
+}
